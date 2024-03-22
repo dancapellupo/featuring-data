74383f7e51a513559061c0b18e9925fbccf23597
Dan Capellupo <dan@dmcdatascience.com>
2024-03-21 22:26:21 -0400

    EDA: Added comments.

--- src/featuringdata/featuresEDA/_initial_eda_functions.py
+++ src/featuringdata/featuresEDA/_initial_eda_functions.py
@@ -4,6 +4,23 @@ import pandas as pd
 
 
 def count_null_values(data_df):
+    """
+    Counts the null values for every column in the input dataframe.
+
+    Parameters
+    ----------
+    data_df : pd.DataFrame
+        The input dataframe.
+
+    Returns
+    -------
+    null_cols_df : pd.DataFrame
+        A dataframe with all the features that have at least one NULL value.
+        The dataframe has the following columns:
+        - "Feature": Name of the data column.
+        - "Num of Nulls": Total number of null values in the column.
+        - "Frac Null": The fraction of all values in that column that are null.
+    """
     null_cols_df = pd.DataFrame(columns=["Feature", "Num of Nulls", "Frac Null"])
 
     null_cols = data_df.columns[data_df.isna().any()].tolist()
@@ -12,12 +29,33 @@ def count_null_values(data_df):
         num_nulls = data_df[col].isna().sum()
         null_cols_df.loc[jj] = col, num_nulls, round(num_nulls / len(data_df), 2)
 
+    # Sort the dataframe by number of NULL values per feature, in descending order:
     null_cols_df = null_cols_df.sort_values(by=["Num of Nulls"], ascending=False)
 
     return null_cols_df
 
 
 def sort_numeric_nonnumeric_columns(data_df, target_col=None):
+    """
+    Sorts the names of the numeric and nun-numeric/categorical columns into
+    two separate lists.
+
+    Parameters
+    ----------
+    data_df : pd.DataFrame
+        The input dataframe.
+
+    target_col : str, default=None
+        The name of the dataframe column containing the target variable.
+
+    Returns
+    -------
+    numeric_cols : List
+        A list of names of columns with numeric values.
+
+    non_numeric_cols : List
+        A list of names of columns with non-numeric / categorical values.
+    """
 
     numeric_cols = data_df.select_dtypes(include='number').columns.to_list()
     non_numeric_cols = data_df.select_dtypes(exclude='number').columns.to_list()
@@ -35,7 +73,32 @@ def sort_numeric_nonnumeric_columns(data_df, target_col=None):
 
 
 def count_numeric_unique_values(data_df, numeric_cols, uniq_vals_thresh=10):
+    """
+    Counts the number of unique values in every numeric column.
 
+    Parameters
+    ----------
+    data_df : pd.DataFrame
+        The input dataframe.
+
+    numeric_cols : List
+        A list of names of columns with numeric values (from the function
+        'sort_numeric_nonnumeric_columns').
+
+    uniq_vals_thresh : int, default=10
+        Any feature with fewer than this number of unique values will be saved
+        to the output dataframe.
+
+    Returns
+    -------
+    numeric_uniq_vals_df : pd.DataFrame
+        A dataframe listing any numeric columns that have no more than
+        "numeric_uniq_vals_thresh" unique values. The dataframe has the
+        following columns:
+        - "Feature": Name of the numeric data column.
+        - "Num Unique Values": The number of unique values in that data
+          column.
+    """
     numeric_uniq_vals_df = pd.DataFrame(columns=["Feature", "Num Unique Values"])
 
     jj = 0
@@ -46,13 +109,39 @@ def count_numeric_unique_values(data_df, numeric_cols, uniq_vals_thresh=10):
             numeric_uniq_vals_df.loc[jj] = col, num_uniq
             jj += 1
 
+    # Sort the dataframe by number of unique values per feature, in ascending order:
     numeric_uniq_vals_df = numeric_uniq_vals_df.sort_values(by=["Num Unique Values"])
 
     return numeric_uniq_vals_df
 
 
 def count_nonnumeric_unique_values(data_df, non_numeric_cols, uniq_vals_thresh=5):
+    """
+    Counts the number of unique values in every non-numeric column.
 
+    Parameters
+    ----------
+    data_df : pd.DataFrame
+        The input dataframe.
+
+    non_numeric_cols : List
+        A list of names of columns with non-numeric values (from the function
+        'sort_numeric_nonnumeric_columns').
+
+    uniq_vals_thresh : int, default=10
+        [Currently unused]
+        Any non-numeric feature with greater than this number of unique values
+        will be reported.
+
+    Returns
+    -------
+    numeric_uniq_vals_df : pd.DataFrame
+        A dataframe listing the number of unique values in every non-numeric
+        column. The dataframe has the following columns:
+        - "Feature": Name of the non-numeric data column.
+        - "Num Unique Values": The number of unique values in that data
+          column.
+    """
     non_numeric_uniq_vals_df = pd.DataFrame(columns=["Feature", "Num Unique Values"])
 
     jj = 0
@@ -63,6 +152,7 @@ def count_nonnumeric_unique_values(data_df, non_numeric_cols, uniq_vals_thresh=5
         non_numeric_uniq_vals_df.loc[jj] = col, num_uniq
         jj += 1
 
+    # Sort the dataframe by number of unique values per feature, in descending order:
     non_numeric_uniq_vals_df = non_numeric_uniq_vals_df.sort_values(by=["Num Unique Values"], ascending=False)
 
     return non_numeric_uniq_vals_df
